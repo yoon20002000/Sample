8a0bc8eba89bb87ffb5490934453294544a24465
yoon20002000 <yoon20002000@naver.com>
2024-10-13 18:19:45 +0900

    Roll back GameMessage and ProtoBuf

--- Source/Sample/Private/GameMessageBase.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "GameMessageBase.h"
-
-// Add default functionality here for any IGameMessageBase functions that are not pure virtual.
--- Source/Sample/Sample.Build.cs
+++ Source/Sample/Sample.Build.cs
@@ -8,6 +8,6 @@ public class Sample : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Sockets", "NetWorking" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Sockets", "Networking" });
 	}
 }
